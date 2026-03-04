e1d4fc2ee10c3158c8ee1612a84749815954d037
Shoaib Qureshi <shoaib.saq@gmail.com>
2026-03-04 15:41:13 +0530

    lottie files removed  v3

--- woocommerce-edition-management.php
+++ woocommerce-edition-management.php
@@ -19,7 +19,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('PMCM_VERSION', '2.5.0');
+define('PMCM_VERSION', '2.6.0');
 define('PMCM_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('PMCM_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('PMCM_PLUGIN_BASENAME', plugin_basename(__FILE__));
